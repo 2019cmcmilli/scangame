121ef9b726a1ecb8dcb44874ac659a4f30d561bb
McMillin <2019cmcmilli@tjhsst.edu>
2019-02-22 10:15:15 -0500

    Created Game Activity files

---
+++ app/src/main/java/com/example/a1412023/scangametest1/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1412023.scangametest1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity{
+    private String TAG = "Activity_Game";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
